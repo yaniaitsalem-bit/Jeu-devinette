ab43aaf05cbd2459b5167f370a329d7500e8f303
AIT SALEM Yani <yaniaitsalem@gmail.com>
2026-03-01 16:42:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from menu import afficher_menu, lire_choix
+from jeu import jouer_partie
+from utils import demander_rejouer
 
 
 def main():
     \"\"\"Boucle principale du jeu.\"\"\"
+    print(\"\nBienvenue dans le Jeu de Devinette !\")
+
     while True:
         afficher_menu()
         choix = lire_choix()
 
         if choix == \"1\":
-            print(\"\n[Lancement d'une nouvelle partie...]\")
+            tentatives, difficulte = jouer_partie()
+            while demander_rejouer():
+                tentatives, difficulte = jouer_partie()
         elif choix == \"2\":
             print(\"\n[Affichage des scores...]\")
         elif choix == \"3\":
